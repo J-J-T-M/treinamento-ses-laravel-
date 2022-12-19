22dda15f5b29675dad8f6594ac0c31d92ab30cdd
J-J-T-M <jonatham.junior.18@gmail.com>
2022-12-19 16:29:25 -0400

    criando a base de dados

---
+++ CADASTRO-PACIENTES-SES/database/migrations/2022_12_19_200628_create_patients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('cpf')->unique();
+            $table->bigInteger('cns')->unique();
+            $table->string('name',255);
+            $table->date('birth');
+            $table->string('email');
+            $table->bigInteger('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+};
