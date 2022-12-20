6b14caa9d38c0f5a07faddeb7e57a0f376e68105
J-J-T-M <jonatham.junior.18@gmail.com>
2022-12-20 16:40:36 -0400

    Trabalhando o CRUD de patients

---
+++ CADASTRO-PACIENTES-SES/resources/views/patients/index.blade.php
@@ -0,0 +1,34 @@
+<h1>Lista de Pacientes</h1>
+<table>
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>cpf</th>
+            <th>cns</th>
+            <th>nome</th>
+            <th>nascimento</th>
+            <th>email</th>
+            <th>telefone</th>
+            <th>cidade</th>
+        </tr>
+    </thead>
+    <tbody>
+@forelse ($patients as $patient)
+    <tr>
+        <td>{{$patient->id}}</td>
+        <td>{{$patient->cpf}}</td>
+        <td>{{$patient->cns}}</td>
+        <td>{{$patient->name}}</td>
+        <td>{{$patient->birth}}</td>
+        <td>{{$patient->email}}</td>
+        <td>{{$patient->phone}}</td>
+        <td>{{$patient->county->name}}-{{$patient->county->fu}}</td>
+    </tr>
+   
+@empty
+<tr>
+
+</tr>
+@endforelse
+    </tbody>
+</table>
